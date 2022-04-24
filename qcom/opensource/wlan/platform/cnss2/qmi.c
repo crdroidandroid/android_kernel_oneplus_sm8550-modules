dd7a47977e6f462614e7997847398f30c75c1e19
LuK1337 <priv.luk@gmail.com>
2022-04-24 10:30:24 +0200

    cnss2: Propagate WLAN MAC to cnss utils
    This allows us to use platform driver MAC address provisioning in
    qcacld-3.0.
    
    Change-Id: I09984f44314316367f25eb514924aedf5f01a464

--- qcom/opensource/wlan/platform/cnss2/qmi.c
+++ qcom/opensource/wlan/platform/cnss2/qmi.c
@@ -13,6 +13,7 @@
 #endif /* OPLUS_FEATURE_WIFI_BDF */
 
 #include "bus.h"
+#include "cnss_utils.h"
 #include "debug.h"
 #include "main.h"
 #include "qmi.h"
@@ -1389,6 +1390,11 @@ int cnss_wlfw_wlan_mac_req_send_sync(struct cnss_plat_data *plat_priv,
 	memcpy(req.mac_addr, mac, mac_len);
 #endif /* OPLUS_FEATURE_WIFI_MAC */
 	req.mac_addr_valid = 1;
+
+	ret = cnss_utils_set_wlan_mac_address(req.mac_addr, mac_len);
+	if (ret < 0) {
+		cnss_pr_err("Failed to set cnss utils wlan mac address (non-fatal), err: %d\n", ret);
+	}
 
 	ret = qmi_send_request(&plat_priv->qmi_wlfw, NULL, &txn,
 			       QMI_WLFW_MAC_ADDR_REQ_V01,
