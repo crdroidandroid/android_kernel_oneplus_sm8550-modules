4a6769cdf72a4da6c1ab1a4a1ab091700a4ddb5d
inferno0230 <mail@inferno0230.in>
2025-09-12 02:34:32 +0530

    oplus: theia: Fix header file include path
    Change-Id: I10b0d656fa47f55ce5c9fac2420aa9a2624d196e
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- oplus/kernel/dfr/common/theia/theia_trace.h
+++ oplus/kernel/dfr/common/theia/theia_trace.h
@@ -75,7 +75,7 @@ TRACE_EVENT(bright_screen_monitor,
 #endif /* _TRACE_THEIA_H */
 
 #undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH ../../../vendor/oplus/kernel/dfr/common/theia
+#define TRACE_INCLUDE_PATH ../../../sm8550-modules/oplus/kernel/dfr/common/theia
 
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_FILE theia_trace
